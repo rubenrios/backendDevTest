b9851308a77fbbfcb34ec93c91955fa0a477ddc8
rubenrios <ruben.rios@udc.es>
2025-11-27 02:16:39 +0100

    Changes in service after detecting errors with product 4

--- products/src/main/java/com/rubenrbr/products/application/service/ProductServiceImpl.java
+++ products/src/main/java/com/rubenrbr/products/application/service/ProductServiceImpl.java
@@ -1,10 +1,13 @@
 package com.rubenrbr.products.application.service;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.rubenrbr.products.domain.exception.ProductNotFoundException;
 import com.rubenrbr.products.domain.model.ProductDetail;
 import com.rubenrbr.products.domain.port.in.ProductService;
 import com.rubenrbr.products.domain.port.out.ProductRepository;
@@ -19,8 +22,18 @@ public class ProductServiceImpl implements ProductService {
 
   @Override
   public Set<ProductDetail> getSimilarProducts(String productId) {
-    return productRepository.getSimilarIds(productId).stream()
-        .map(productRepository::getProductDetail)
+    List<String> ids = productRepository.getSimilarIds(productId);
+
+    return ids.stream()
+        .map(
+            id -> {
+              try {
+                return productRepository.getProductDetail(id);
+              } catch (ProductNotFoundException e) {
+                return null;
+              }
+            })
+        .filter(Objects::nonNull)
         .collect(Collectors.toSet());
   }
 }
